25b2aa84f2ca4f9f5a45629dea129f1b4af71977
jackey925 <jackey925@163.com>
2019-08-21 16:09:17 +0800

    change printInfo

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main()  {
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Hello kubernetes %q", html.EscapeString(r.URL.Path))
 	})
 	log.Fatal(http.ListenAndServe(":9200", nil))
 }
