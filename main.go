9ce8d0bf26ccf7d7e1c4957ef2a406ffeb29aa0e
jee <232485230@qq.com>
2019-11-04 16:02:15 +0800

    main.go

--- main.go
+++ main.go
@@ -11,6 +11,5 @@ func main()  {
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello kubernetes %q", html.EscapeString(r.URL.Path))
 	})
-	
 	log.Fatal(http.ListenAndServe(":9200", nil))
 }
