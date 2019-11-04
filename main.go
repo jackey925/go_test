c833263c35cc4b4cfa2e6281d832c85756fc5156
jee <232485230@qq.com>
2019-11-04 16:19:27 +0800

    host

--- main.go
+++ main.go
@@ -9,7 +9,10 @@ import (
 
 func main()  {
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello kubernetes %q", html.EscapeString(r.URL.Path))
+
+		fmt.Fprintf(w, "Hello kubernetes %q\n", html.EscapeString(r.URL.Host))
 	})
 	log.Fatal(http.ListenAndServe(":9200", nil))
 }
+
+
